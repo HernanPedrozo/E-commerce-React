299f5bdd033dfc7c671372261dd4bee77e15369c
Kevin Hernán Pedrozo <hernanpedrozo5@gmail.com>
2023-08-15 23:16:36 -0300

    Se instalo el rect router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import Header from './components/Header/Header'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
 
 
 function App() {
 
   return (
-    <>
-      <Header/>
-      <ItemListContainer greeting='Catalogo'/>  
-    </>
+    <div className='App'>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<ItemListContainer greeting='Catalogo'/> }/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
